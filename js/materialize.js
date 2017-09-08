fa35e6ea4f25d35c70367beda57e6377b5b97458
Antony Kurniawan <Smankusors@users.noreply.github.com>
2017-09-08 16:39:23 +0700

    Materialize update
    Better login dialog

--- js/materialize.js
+++ js/materialize.js
@@ -1,9 +1,11 @@
-$("body").append('<div class="search-mat-fab-btn"><img src="images/icon-search.png" id="search-mat-btn" /></div><div id="dark_bg">&nbsp;</div><div id="search-mat-box"><div class="dialog-title">Pencarian</div><input id="cari" type="text" class="search-mat-text" placeholder="Cari" /><div class="action-bar-mat"><input value="BATAL" type="submit" id="search-cancel" class="submit-mat-btn" /><input value="CARI" type="submit" id="search" class="submit-mat-btn" /></div></div><div class="theme-footer">SIM Material Theme oleh Smankusors (Antony Kurniawan)</div>');
+$("#login-box").remove();
+$("body").append('<div class="search-mat-fab-btn"><img src="images/icon-search.png" id="search-mat-btn" /></div><div id="dark_bg" style="display:none;">&nbsp;</div><div id="search-mat-box" class="dialog" style="display:none;"><div class="dialog-title">Pencarian</div><div class="dialog-content"><input id="cari" type="text" class="search-mat-text" placeholder="Cari" /></div><input value="CARI" type="submit" id="search" class="dialog-btn" /></div>');
+$("body").append('<div id="login-dialog" class="dialog" style="display:none"><div class="dialog-header"> <img src="http://smankusors.16mb.com/private/sim_small_logo.png" /><div class="progress" style="display:none;"><div class="indeterminate" /></div></div><div class="dialog-content"> <input name="user" id="user" type="text" placeholder="Username" /> <input id="pass" name="pass" type="password" placeholder="Password" /><p class="status">&nbsp;</p></div> <input value="LOGIN" id="doLogin-btn" name="login" type="button" class="dialog-btn" /></div>');
 $(".menu td:first-child li").toggleClass("selected", true);
+
 $("#search-btn").remove();
 $("#search-box").remove();
-$("#search-mat-box").hide();
-$("#dark_bg").hide();
+$('.banner-shell').removeAttr("style");
 $(".menu td li, .child").click(function(){
 	$(".body-mid-container").hide();
 	$(".body-left-container").show();
@@ -13,14 +15,15 @@ $(".menu td li, .child").click(function(){
 		if (href=="http://perpustakaan.stts.edu") window.open(href,'_blank');
 		else if (href == "update_waktu_bau_banotomatis.php" && ($("#page-updatewaktubaubanotomatis").length)) {}
 		else {
-			$(".body-left-container").fadeTo(200, 0.1);
 			$('.menu').find('li').toggleClass("selected", false);
 			$(this).toggleClass("selected", true);
 			$("#ui-datepicker-div").remove();
-			$(".body-left").load(href, function () {
-				if (href != 'pengumuman.php') $("#div-pengumumanGambar").hide();
-				else $("#div-pengumumanGambar").show();
-				$(".body-left-container").fadeTo(200, 1);
+			$(".body-left-container").fadeTo(200, 0, function() {
+				$(".body-left").load(href, function () {
+					if (href != 'pengumuman.php') $("#div-pengumumanGambar").hide();
+					else $("#div-pengumumanGambar").show();
+					$(".body-left-container").fadeTo(200, 1);
+				});
 			});
 		}
 	}
@@ -32,24 +35,53 @@ $('.search-mat-fab-btn').click(function (){
 });
 $('#search-cancel, #dark_bg').click(function() {
 	$('#search-mat-box').fadeOut(200);
+	$('#login-dialog').fadeOut(200);
 	$('#dark_bg').fadeOut(200);
 });
-$('#search').click(function () {
-	$(".body-left-container").fadeTo(200, 0.1);
-	$(".body-left").load("pengumuman.php?cari=" + $("#cari").val().replace(" ", "xxx"), function() {
-		$('#search-mat-box').fadeOut(200);
-		$('#dark_bg').fadeOut(200);
-		$(".body-left-container").fadeTo(200, 1);
-	});
+$('#login-btn').unbind('click');
+$('#login-btn').click(function (){
+	$('#login-dialog').fadeIn(200);
+	$('#dark_bg').fadeTo(200, 0.5);
 });
-$('#search-mat-box').keypress(function (e) {
-	if (e.which == 13) {
-		$(".body-left-container").fadeTo(200, 0.1);
+function doSearch() {
+	$(".body-left-container").fadeTo(200, 0, function() {
 		$(".body-left").load("pengumuman.php?cari=" + $("#cari").val().replace(" ", "xxx"), function() {
 			$('#search-mat-box').fadeOut(200);
 			$('#dark_bg').fadeOut(200);
 			$(".body-left-container").fadeTo(200, 1);
 		});
-		return false;
-	}
+	});
+}
+function doLogin() {
+	$('.dialog-header .progress').fadeIn(200);
+	$('#doLogin-btn').prop('disabled', true);
+	$('.status').fadeOut(200);
+	$.post("cek_login.php", { user: $("#user").val(), pass: $("#pass").val()}, function(result){
+		$('.dialog-header .progress').fadeOut(200);
+		if (result == "<script>window.location='index.php'</script>") {
+			/*
+			history.pushState(null, "", "index.php");
+			$('html').fadeOut(200, function() {
+				$.get("index.php", function(index) {
+					$('html').html($(index).html());
+					console.log($(index).html());
+					$('html').fadeIn(200);
+				});
+			});
+			*/
+			location.href = "index.php";
+		} else {
+			$('#doLogin-btn').prop('disabled', false);
+			$('.status').fadeIn(200);
+			$('.status').html(result);
+		}		
+	});
+}
+$('#search').click(function () { doSearch(); });
+$('#search-mat-box').keypress(function (e) {
+	if(e.which == 13 || e.keyCode == 13) doSearch();
 });
+$('#login-dialog').keypress(function (e) {
+	if(e.which == 13 || e.keyCode == 13) doLogin();
+}); 
+$('#doLogin-btn').click(function (){ doLogin(); });
